1eff81a7b49b971634aea5f0bf50270b0d9804d7
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2023-04-10 20:34:45 -0600

    main screen and safe area style for ios & android

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from '../theme/appTheme'
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Test calculator main screen</Text>
+    </View>
+
+  )
+}
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    text: {
+        color: 'white',
+        fontSize: 30,
+        textAlign: 'right',
+    },
+});
+
+export default styles;
